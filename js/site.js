7b462fe28e16ec790cf889004cf3af3909f12f10
Heidi El-Hosaini <heidi.elhosaini@gmail.com>
2017-12-04 14:50:32 +0000

    Changes to dropdown selections
    - inserted styled divs on top of (hidden) select elements - due to the fact that the OS uses default styles for dropdowns so for some people it appears blank

--- js/site.js
+++ js/site.js
@@ -1,21 +1,24 @@
-///////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
 //Next dev steps:
-// - remove "var data ="" from data.js
+// - remove "var data ="" from data.js  (or add it in export from database?)
 // - barchart:
 //    - on hover tooltip for whole height of chart, not only for bars  https://blog.webkid.io/responsive-chart-usability-d3/
 //    - epiweek date on Thurs 00:00, should be Thurs 12:00 to be exactly halfway through week
-// - buttons for selecting relative time range options? e.g. 'last 1 epimonth', 'last 8 epiweeks'
 //
 // Recommendations from others:
 // - Help Intro.js
 //
+// Suggestions:
+// - add buttons for selecting relative time range options? e.g. 'last 1 epimonth', 'last 8 epiweeks'
+// - move Nombre de Cas/lethality dropdown choices to above map (as only relevant to map)
+//
 // Done since last update:
-// - added lethality layer to map & histogram
-// - brush chart default time selection - relfects relative time (so far used 'last 3 months' as default)
-// - x-axis shows epiweek (not date)
-// - add prov overlay to zones view 
-// - make zone/prov buttons more aesthetic  
-///////////////////////////////////////////////////
+// - added lethality layer to map & bar chart
+// - brush chart default time selection relfects relative time (uses 'last 3 months' as default)
+// - x-axis label displays epiweek (not date) & calculates label frequency depending on bar width
+// - added province boundary to zones view in map
+// - made zone/prov buttons more aesthetic  
+////////////////////////////////////////////////////////////////////
 
 // - examples online: http://techslides.com/over-2000-d3-js-examples-and-demos
 // - brush handles: https://bl.ocks.org/mbostock/4349545
@@ -951,7 +954,7 @@ function getCurrentTimeSeriesData() {
 	} else if (g.currentvars.currentStat.abrv=='dec') {
 		timeSeries = cf.deathsByEpiDateGroup.top(Infinity);
 		//timeSeries = cf.lethalityByEpiDateGroup.top(Infinity);
-	} else if (g.currentvars.currentStat.abrv=='let') {	*/			//HEIDI change here for leth
+	} else if (g.currentvars.currentStat.abrv=='let') {	*/			
 		statsInTime = cf.statsByEpiDateGroup.top(Infinity);
 		//timeSeries = cf.lethalityByEpiDateGroup.top(Infinity);
 	//};
@@ -2097,6 +2100,11 @@ window.onload = function () {
 var select_disease = document.getElementById("disease-select");
 for (disease in g.diseaseList) {
 	//console.log("disease: ", disease, g.diseaseList[disease]);
+
+	/*opt = new Option(g.diseaseList[disease], disease);
+    opt.className = "opt";
+    select_disease.options[select_disease.options.length] = opt;*/
+
 	if (g.diseaseList[disease]!='') {
     	select_disease.options[select_disease.options.length] = new Option(g.diseaseList[disease], disease);
     }
@@ -2109,6 +2117,85 @@ for (stat in g.statList) {
     	select_stat.options[select_stat.options.length] = new Option(g.statList[stat].full, stat);
     }
 }
+
+// Iterate over each select element
+$('select').each(function () {
+
+    // Cache the number of options
+    var $this = $(this),
+        numberOfOptions = $(this).children('option').length;
+
+	console.log($this, numberOfOptions);
+
+    // Hides the select element
+    $this.addClass('s-hidden');
+
+    // Wrap the select element in a div
+    $this.wrap('<div class="select"></div>');
+
+    // Insert a styled div to sit over the top of the hidden select element
+    $this.after('<div class="styledSelect"></div>');
+
+    // Cache the styled div
+    var $styledSelect = $this.next('div.styledSelect');
+
+    // Show the first select option in the styled div
+    $styledSelect.text($this.children('option').eq(0).text());
+
+    // Insert an unordered list after the styled div and also cache the list
+    var $list = $('<ul />', {
+        'class': 'options'
+    }).insertAfter($styledSelect);
+
+
+    // Insert a list item into the unordered list for each select option
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+
+    // Cache the list items
+    var $listItems = $list.children('li');
+
+    // Show the unordered list when the styled div is clicked (also hides it if the div is clicked again)
+    $styledSelect.click(function (e) {
+        e.stopPropagation();
+        $('div.styledSelect.active').each(function () {
+            $(this).removeClass('active').next('ul.options').hide();
+        });
+        $(this).toggleClass('active').next('ul.options').toggle();
+    });
+
+    // Hides the unordered list when a list item is clicked and updates the styled div to show the selected list item
+    // Updates the select element to have the value of the equivalent option
+    $listItems.click(function (e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('active');
+        $this.val($(this).attr('rel'));
+        $list.hide();
+        //console.log($this.val(), $styledSelect.text(), $styledSelect.context.id);
+        if (($styledSelect.context.id == 'disease-select') && (g.currentvars.currentDisease != $styledSelect.text())) {
+        	g.currentvars.currentDisease = $styledSelect.text();
+        	changeDiseaseSelection();
+        	console.log($this.val(), $styledSelect.text(), $styledSelect.context.id);
+        } else if (($styledSelect.context.id == 'stat-select') && (g.currentvars.currentStat.full != $styledSelect.text())) {
+        	g.currentvars.currentStat = $styledSelect.text();
+        	changeStatSelection();
+        	console.log($this.val(), $styledSelect.text(), $styledSelect.context.id);
+        };
+    });
+
+    // Hides the unordered list when clicking outside of it
+    $(document).click(function () {
+        $styledSelect.removeClass('active');
+        $list.hide();
+    });
+
+});
+
+
 
 
 function changeDiseaseSelection() {
